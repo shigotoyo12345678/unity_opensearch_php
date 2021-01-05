7b6fecb811d481de4fc2d565064ece53642848ca
shigotoyo12345678 <73812038+shigotoyo12345678@users.noreply.github.com>
2021-01-05 22:43:58 +0900

    Add files via upload

--- sql.php
+++ sql.php
@@ -4,35 +4,29 @@ require_once('dbconnect.php');
 $pdo = connectDB();
 
 //POSTうけとり
-$prefecture = $_POST["prefecture"]; //県
-$city = $_POST["city"];             //市
-//$industry = $_SESSION["industry"];     //業種
-$shopName = $_POST["name"];     //店名
-$url = $_POST["url"];               //URL
-$post = $_POST["post"];             //郵便番号
-$addr11 = $_POST["address"];         //住所
-$tel = $_POST["tel"];               //電話番号
-/*
-$a = $_SESSION["THOH"] . ":" . $_SESSION["THOM"];             //通常開店時間（平日）
-$b = $_SESSION["THCH"] . ":" . $_SESSION["THCM"];             //通常閉店時間（平日）
-$c = $_SESSION["TYOH"] . ":" . $_SESSION["TYOM"];             //通常開店時間（土日祝）
-$d = $_SESSION["TYCH"] . ":" . $_SESSION["TYCM"];             //通常閉店時間（土日祝）*/
-$e = $_POST["open1"];             //特別開店時間（平日）
-$f = $_POST["close1"];            //特別閉店時間（平日）
-$g = $_POST["open2"];              //特別開店時間（土日祝）
-$h = $_POST["close2"];          //特別閉店時間（土日祝）
-$start = $_POST["start"];           //特別営業時間適用開始日時
-$end = $_POST["end"];               //特別営業時間適用終了日時
+$prefecture = $_POST["prefecture"];   //県
+$city = $_POST["city"];               //市
+$shopName = $_POST["name"];           //店名
+$url = $_POST["url"];                 //URL
+$post = $_POST["post"];               //郵便番号
+$addr11 = $_POST["address"];          //住所
+$tel = $_POST["tel"];                 //電話番号
+$e = $_POST["open1"];                 //特別開店時間（平日）
+$f = $_POST["close1"];                //特別閉店時間（平日）
+$g = $_POST["open2"];                 //特別開店時間（土日祝）
+$h = $_POST["close2"];                //特別閉店時間（土日祝）
+$start = $_POST["start"];             //特別営業時間適用開始日時
+$end = $_POST["end"];                 //特別営業時間適用終了日時
 //一時閉店
 if (isset($_SESS_POSTION["close"])) {
     $close = 1;
 } else {
     $close = 0;
 }
-$remarks = $_POST["remark"];       //備考
-$tag = $_POST["tag"];               //タグ
+$remarks = $_POST["remark"];          //備考
+$tag = $_POST["tag"];                 //タグ
 if (isset($_POST["image"])) {
-    $image = $_POST["image"];           //画像
+    $image = $_POST["image"];         //画像
 } else {
     $image = "";
 }
